3318b29eb093b617d5be6987e9041d6131912e2b
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-06-12 18:50:27 +0200

    feat: add api of PoortfolioController and verified in Postman the link if the file is a json

---
+++ app/Http/Controllers/Api/PortfolioController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Portfolio;
+
+class PortfolioController extends Controller
+{
+    public function index() {
+        $portfolios = Portfolio::all();
+        
+        return response()->json($portfolios);
+    }
+}
